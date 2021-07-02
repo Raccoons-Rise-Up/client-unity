f0c6bac50e935eb1881a6ff0f2ee7031b935abce
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-07-02 11:43:27 -0500

    ENet Network

---
+++ Assets/Scripts/ENetClient.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ENet;
+using Common.Networking.Packet;
+
+namespace GameClient.Networking 
+{
+    public class ENetClient : MonoBehaviour
+    {
+        public const byte CHANNEL_ID = 0;
+
+        public static Peer Peer { get; set; }
+
+        public string ip = "127.0.0.1";
+        public ushort port = 3000;
+
+        public bool active = true;
+
+        private void Start()
+        {
+            ENet.Library.Initialize();
+
+            using Host client = new Host();
+            var address = new Address();
+
+            address.SetHost(ip);
+            address.Port = port;
+            client.Create();
+
+            Peer = client.Connect(address);
+
+            while (active)
+            {
+                var polled = false;
+
+                while (!polled)
+                {
+                    if (client.CheckEvents(out ENet.Event netEvent) <= 0)
+                    {
+                        if (client.Service(15, out netEvent) <= 0)
+                            break;
+
+                        polled = true;
+                    }
+
+                    switch (netEvent.Type)
+                    {
+                        case ENet.EventType.None:
+                            break;
+
+                        case ENet.EventType.Connect:
+                            Debug.Log("Client connected to server");
+                            break;
+
+                        case ENet.EventType.Disconnect:
+                            Debug.Log("Client disconnected from server");
+                            break;
+
+                        case ENet.EventType.Timeout:
+                            Debug.Log("Client connection timeout");
+                            break;
+
+                        case ENet.EventType.Receive:
+                            Debug.Log("Packet received from server - Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+                            netEvent.Packet.Dispose();
+                            break;
+                    }
+                }
+
+                client.Flush();
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R)) 
+            {
+                var sendPacket = new ClientPacket(ClientPacketType.Disconnect);
+                ENetNetwork.Send(sendPacket, PacketFlags.Reliable);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ENetNetwork.cs
@@ -0,0 +1,16 @@
+using ENet;
+
+using Common.Networking.Packet;
+
+namespace GameClient.Networking
+{
+    public class ENetNetwork
+    {
+        public static void Send(GamePacket gamePacket, PacketFlags packetFlags)
+        {
+            var packet = default(ENet.Packet);
+            packet.Create(gamePacket.Data, packetFlags);
+            ENetClient.Peer.Send(ENetClient.CHANNEL_ID, ref packet);
+        }
+    }
+}
